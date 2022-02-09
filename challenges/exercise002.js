f81c1b2c8b4c4fe2193a11e24d8bf588fb235f02
Lakshmi R Pillai <luxmi.r.pillai@gmail.com>
2022-02-09 01:34:32 +0000

    Completed excercise002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,51 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
   // Your code here!
+  var fillings=sandwich.fillings;
+  //console.log(fillings);
+  return fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  var loc=person.city;
+  if (loc=='Manchester') return true;
+  else return false;
+
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
   // Your code here!
+  var numOfBuses=Math.floor(people/40);// number of buses =people /40 (completely divisible)+remaining people in another bus
+  if (people% 40!=0) return numOfBuses+1;
+  else return numOfBuses;
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
   // Your code here!
+  var len=arr.length;
+  var countSheep=0;
+  for (var i=0;i<len;i++){
+    if (arr[i].toLowerCase()=='sheep')
+     countSheep+=1;
+  }
+  //console.log(countSheep);
+  return countSheep;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  var address=person.address;
+  var postCode=address.postCode;
+  var city=address.city;
+  if (postCode.charAt(0)=='M') 
+    if (city=='Manchester')  return true;
+    else return false;
+  else return false;
 }
 
 module.exports = {
